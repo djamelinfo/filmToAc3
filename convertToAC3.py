784cc96024d0679ee5dd9d167abde06e0fbf95a0
djamelinfo <dd>
2020-04-07 00:34:46 +0200

    rename file to replace special characters in name, refacto, nice output with colors

--- convertToAC3.py
+++ convertToAC3.py
@@ -1,28 +1,44 @@
 import os
 import subprocess
 import sys
+import re
+from colorama import init
+from colorama import Fore, Back, Style
 
-path = sys.argv[1]
-fileFormat = "."+sys.argv[2]  # .mkv, .mp4, .avi, .mpeg, whatever.
 
-os.system("mkdir -p " + path + "convertedFilms")
+def replace_special_characters(filename):
+    return re.sub(r'\.+', ".", re.sub('[^a-zA-Z0-9\n\.]', '.', filename))
 
-for filename in os.listdir(path):
 
-    print("List of no ac3 file in " + path)
+def main():
+    init()
+    path = sys.argv[1]
+    fileFormat = "." + sys.argv[2]  # .mkv, .mp4, .avi, .mpeg, whatever.
 
-    if os.path.isfile(path + filename) & filename.endswith(fileFormat):
-        # print(filename)
-        output = subprocess.check_output("ffprobe -v error -show_streams " + path + "{0}".format(filename),shell=True)
-        if "ac3" not in output.decode("utf-8"):
-            print(output)
-            distFileName = filename.replace(fileFormat, ".ac3"+fileFormat)
-            print("src : " + filename + "\ndist :" + distFileName)
-            os.system("ffmpeg -v 1 -i " + path + filename + " -map 0 -vcodec copy -scodec copy -acodec ac3 -b:a 640k " + path + distFileName)
-            print("move file to " + path + "convertedFilms")
-            os.system("mv " + path + filename + " " + path + "convertedFilms/")
+    os.system("mkdir -p " + path + "convertedFilms")
+
+    print("Convert no ac3 file in " + path)
+
+    for filename in os.listdir(path):
+        if os.path.isfile(path + filename) and filename.endswith(fileFormat):
+            src_file = os.path.join(path, filename)
+            renamed_file = os.path.join(path, replace_special_characters(filename))
+
+            print("Rename File: '" + Fore.GREEN + src_file + Style.RESET_ALL + "'\nto :" + Fore.GREEN + renamed_file + Style.RESET_ALL)
+            os.rename(src_file, renamed_file)
+            output = subprocess.check_output("ffprobe -v error -show_streams {0}".format(renamed_file), shell=True)
+            if "ac3" not in output.decode("utf-8"):
+                print(Fore.RED + "The audio track of " + renamed_file + ", will be converted to AC3, with 5.1 channels" + Style.RESET_ALL)
+                converted_file = renamed_file.replace(fileFormat, ".ac3" + fileFormat)
+                print("src : " + renamed_file + "\ndist :" + converted_file)
+                os.system("ffmpeg -v 1 -i " + renamed_file + " -map 0 -vcodec copy -scodec copy -acodec ac3 -b:a 640k " + converted_file)
+                print("Move the converted file to " + path + "convertedFilms")
+                os.system("mv " + renamed_file + " " + path + "convertedFilms/")
+            else:
+                continue
         else:
             continue
-    else:
-        continue
-    
+
+
+if __name__ == "__main__":
+    main()
